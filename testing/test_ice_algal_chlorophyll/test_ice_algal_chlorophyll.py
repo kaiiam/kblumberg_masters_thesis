46182f52737999259be357ede3874efd92086628
Kai <kblumber@mpi-bremen.de>
2017-11-30 16:37:32 +0100

    added test_ice_algal_chlorophyll directory to test querying against sparql endpoint and using results to filter for data in local datastore

---
+++ testing/test_ice_algal_chlorophyll/test_ice_algal_chlorophyll.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python
+
+from SPARQLWrapper import SPARQLWrapper, JSON
+
+import rdflib
+import rdfextras
+import rdflib.graph as g
+rdfextras.registerplugins() # so we can Graph.query()
+
+#sparql queries:
+#query against ontobee endpoint to get subclasses of sea ice.
+with open('query_1.rq','r') as f_open:
+    query_1 = f_open.read()
+#an intermediate query to be able to retrieve data based on having the term
+#multiyear ice
+with open('query_2.rq','r') as f_open:
+    query_2 = f_open.read()
+#query contains most of the code from query_2 but missing the values
+#section which will be populated from the results of query_1
+with open('query_3.rq','r') as f_open:
+    query_3 = f_open.read()
+
+# wrap the dbpedia SPARQL end-point
+#endpoint = SPARQLWrapper("http://dbpedia.org/sparql")
+#this one is the test ontobee endpoint and doesn't work for real queries
+#endpoint = SPARQLWrapper("http://www.ontobee.org/sparql/")
+
+endpoint = SPARQLWrapper("http://sparql.hegroup.org/sparql/")
+
+# set the query string
+endpoint.setQuery(query_1)
+# select the return format (e.g. XML, JSON etc...)
+endpoint.setReturnFormat(JSON)
+# execute the query and convert into Python objects
+# Note: The JSON returned by the SPARQL endpoint is converted to nested Python dictionaries, so additional parsing is not required.
+results = endpoint.query().convert()
+
+#  make a list of the classes collected from the first query
+str_list = []
+
+for res in results["results"]["bindings"] :
+   str_list.append(res['s']['value'])
+
+# query within local file
+
+#initialize the ConjunctiveGraph which will function as the entire datastore
+graph = g.ConjunctiveGraph()
+
+graph.parse('ice_algal_chlorophyll.nt', format='ntriples')
+graph.parse('ice_algal_chlorophyll.ttl', format='ttl')
+#print graph.serialize(format='ttl')
+
+#query_3 = query_3 + '(<http://purl.obolibrary.org/obo/ENVO_03000073>)}}'
+
+s = '>)(<'
+#print s.join(str_list)
+query_3 = query_3 + '(<' + s.join(str_list) + '>)}}'
+#print query_3
+
+results = graph.query(query_3)
+
+# for row in results:
+#     print "%s" % row
+
+for row in results:
+    print "%s | %s" % row
+
+# for row in results:
+#     print "%s | %s | %s" % row

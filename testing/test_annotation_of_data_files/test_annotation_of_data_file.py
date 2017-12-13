b54edeead2102a2701cb007da8f1f813a2ab64e2
Kai <kblumber@mpi-bremen.de>
2017-12-13 12:00:59 +0100

    tested how to annotate and query datasets to go into the datastore see query_for_data_about_csv_and_columns_2.rq and example_annotations/example_annotations.ttl the latter a template for data annotation

--- testing/test_annotation_of_data_files/test_annotation_of_data_file.py
+++ testing/test_annotation_of_data_files/test_annotation_of_data_file.py
@@ -8,15 +8,29 @@ import rdflib.graph as g
 rdfextras.registerplugins() # so we can Graph.query()
 
 
-with open('test_query_for_equivlance_class_about.rq','r') as f_open:
-    query_1 = f_open.read()
+# with open('test_query_for_equivlance_class_about.rq','r') as f_open:
+#     query_1 = f_open.read()
 
-with open('test_query_for_input_equivalence_class_about.rq','r') as f_open:
-    query_2 = f_open.read()
+# with open('test_query_for_input_equivalence_class_about.rq','r') as f_open:
+#     query_2 = f_open.read()
 
 with open('column_query_annotated.rq','r') as f_open:
     query_3 = f_open.read()
 
+# with open('query_deeply_annotated_column.rq','r') as f_open:
+#     query_4 = f_open.read()
+#this didn't work
+# with open('query_deeply_annotated_column_values.rq','r') as f_open:
+#     query_5 = f_open.read()
+
+# with open('query_deeply_annotated_column_unions.rq','r') as f_open:
+#     query_6 = f_open.read()
+
+# with open('query_for_data_about_csv_and_columns.rq','r') as f_open:
+#     query_7 = f_open.read()
+
+with open('query_for_data_about_csv_and_columns_2.rq','r') as f_open:
+    query_8 = f_open.read()
 
 
 
@@ -26,21 +40,19 @@ graph = g.ConjunctiveGraph()
 #graph.parse('global_chlorophyll_a.nt', format='ntriples')
 #graph.parse('global_chlorophyll_a.ttl', format='ttl')
 
-
-
 graph.parse('datastore.ttl', format='ttl')
 
 #print graph.serialize(format='ttl')
 
-#print query_3
+#print query_5
 
-results = graph.query(query_3)
+results = graph.query(query_8)
 
 # for row in results:
 #     print "%s" % row
 
-    # for row in results:
-    #     print "%s | %s" % row
-
 for row in results:
-    print "%s | %s | %s" % row
+    print "%s | %s" % row
+
+# for row in results:
+#     print "%s | %s | %s" % row

74b91b04ba079ab11879c47cf1517670d86be296
Kai <kblumber@mpi-bremen.de>
2017-12-21 12:45:37 +0100

    made vocamp folder added annotation ttl files and updated queries

---
+++ testing/test_annotation_of_data_files/build_datastore_annotations_and_queries/annotation_of_data_files.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+
+from SPARQLWrapper import SPARQLWrapper, JSON
+
+import rdflib
+import rdfextras
+import rdflib.graph as g
+rdfextras.registerplugins() # so we can Graph.query()
+
+
+with open('query_annotation_of_data_files.rq','r') as f_open:
+    query_1 = f_open.read()
+
+
+#initialize the ConjunctiveGraph which will function as the entire datastore
+graph = g.ConjunctiveGraph()
+
+#graph.parse('global_chlorophyll_a.nt', format='ntriples')
+#graph.parse('global_chlorophyll_a.ttl', format='ttl')
+
+graph.parse('datastore.ttl', format='ttl')
+
+results = graph.query(query_1)
+
+# for row in results:
+#     print "%s" % row
+
+for row in results:
+    print "%s | %s" % row
+
+# for row in results:
+#     print "%s | %s | %s" % row
